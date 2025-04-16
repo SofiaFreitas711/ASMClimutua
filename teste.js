30c369eccb5f54ffceda9b90c57f04074830dd05
SofiaFreitas711 <anasofiafreitas711@gmail.com>
2025-04-16 16:50:08 +0100

    menus e toggles direitos

--- teste.js
+++ teste.js
@@ -83,15 +83,50 @@ function irPagina(params1, params2){
 function abrirMenu(element){
     console.log("cliquei");
     
-    let botao = element
+    let botao = element    
     let li = botao.closest("li")
     let submenu = li.querySelector(".listaSubcategorias")
 
-    let aberto = submenu.classList.toggle("aberto")
-    botao.setAttribute("aria-expanded", aberto? true: false)
-    submenu.setAttribute("aria-hidden", aberto?true:false)
-    botao.setAttribute("aria-label", aberto? "Fechar o menu expansível":"Abrir o menu espansível")
-    console.log(botao);
-    
+    let todosBotoes = document.querySelectorAll("nav .listaDetalhe > button")
+    let todosSubmenus = document.querySelectorAll("nav .listaSubcategorias")
+    // let todosSvg = document.querySelectorAll(".listaDetalhe > svg")
+    // let todosPath = todosBotoes.querySelectorAll("path")
+    let estavaAberto = submenu.classList.contains("aberto")
+
+    for(let btn of todosBotoes){
+        btn.setAttribute("aria-expanded", false)
+        btn.setAttribute("aria-label", "Abrir o menu expansível")
+        let seta = btn.querySelector("path")
+        seta.setAttribute("d","M19.5 8.25l-7.5 7.5-7.5-7.5")
+    }
+
+    for(let menu of todosSubmenus){
+        menu.classList.remove("aberto")
+        menu.setAttribute("aria-hidden", true)
+    }
+
+    if(!estavaAberto){
+        let aberto = submenu.classList.toggle("aberto")
+        botao.setAttribute("aria-expanded", aberto? true: false)
+        submenu.setAttribute("aria-hidden", aberto?true:false)
+        botao.setAttribute("aria-label", aberto? "Fechar o menu expansível":"Abrir o menu espansível")
+
+        let svg = botao.querySelector("svg")
+        let path = svg.querySelector("path")            
+
+        path.setAttribute("d", aberto? "M4.5 15.75l7.5-7.5 7.5 7.5":"M19.5 8.25l-7.5 7.5-7.5-7.5")  
+        
+        setTimeout(function(){
+            submenu.classList.remove("aberto")
+            botao.setAttribute("aria-expanded", false)
+            submenu.setAttribute("aria-hidden",true)
+            botao.setAttribute("aria-label","Abrir o menu espansível")
+            path.setAttribute("d","M19.5 8.25l-7.5 7.5-7.5-7.5")
+        },20000)
+    }
+
+     
+
+
     
 }
